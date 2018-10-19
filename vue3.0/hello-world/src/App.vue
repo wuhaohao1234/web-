5276a6a548dabdcce55f743c0c60680196015ee4
abu <1611499758@qq.com>
2018-10-19 22:59:55 +0800

    vue3.0

---
+++ vue3.0/hello-world/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="第一个Vue3.0实例"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from './components/HelloWorld.vue';
+
+@Component({
+  components: {
+    HelloWorld,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+
+</style>
---
+++ vue3.0/hello-world/src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <input type="text" v-model="myDate" >
+    {{myDate}}
+    <button @click="myClick" >点击我</button>
+    <hr>
+    <ul>
+      <li v-for="item in myDateArr" v-bind:key ="item.text" >
+        {{item.text}}
+      </li>
+    </ul> 
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+  myDate:string = '吴昊'
+  myDateArr = [
+    {text:'第一组数据'},
+    {text:'第二组数据'},
+    {text:'第三组数据'},
+    {text:'第四组数据'},
+    {text:'第五组数据'},
+  ]
+  myClick() {
+    console.log(this.myDate)
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
